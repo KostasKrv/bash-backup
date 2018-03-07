bfa38446395896da403e1b5625cacebf8276761c
Konstantinos Krevatas <297mxq@gmail.com>
2018-03-07 12:36:24 +0200

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,4 +1,5 @@
-## Run command as .\xxxxxx.sh -u 'YOUR_DB_USER' -p 'YOUR_DB_PASSWORD' ##
+## This script will dump all databases one by one, inside a folder ##
+## Run command as .\xxxxxx.sh -u 'YOUR_DB_USER' -p 'YOUR_DB_PASSWORD' -h 'YOUR_DB_HOST(OPTIONAL)' ##
 
 NOW=$(date +"%Y%m%d%H%M%S");
 
@@ -14,7 +15,11 @@ EXECUTE=true;
 ## Maximum history states to be saved ##
 HISTORY_STATES=9
 
+## Do not touch ##
 BACKUP_SAVE_FOLDER="$BACKUP_BASEFOLDER/$NOW"
+
+## Default value if not set by argumnets ##
+MHOST="127.0.0.1"
 
 ## Get the password from argument ##
 while getopts u:p: option
@@ -22,40 +27,41 @@ while getopts u:p: option
 		case "${option}"
 	in
 		u) MUSER=${OPTARG};;
-        p) MPASS=${OPTARG};;
+		p) MPASS=${OPTARG};;
+		h) MHOST=${OPTARG};;
 	esac
 done
 
 ## Create the temporary folder ##
 if [ ! -d "$BACKUP_SAVE_FOLDER" ]; then
-    if [ $EXECUTE = true ]; then    
+	if [ $EXECUTE = true ]; then    
 		mkdir $BACKUP_SAVE_FOLDER
-    else
-    	echo mkdir $BACKUP_SAVE_FOLDER
-    fi    
+	else
+		echo mkdir $BACKUP_SAVE_FOLDER
+	fi
 fi
 
-MHOST="127.0.0.1"
+
  
 ## Get database list ##
 DBS="$(mysql -u $MUSER -h $MHOST -p$MPASS -Bse 'show databases')"
 for db in $DBS
 do
 	## Exlude databases ##
-    if [ $db = 'SKIP_THIS_DATABASE' ] || [ $db = 'OR_SKIP_THIS_DATABASE' ]; then
-    	echo "****** Skipping database $db ******"
-        continue
-    fi    
+	if [ $db = 'SKIP_THIS_DATABASE' ] || [ $db = 'OR_SKIP_THIS_DATABASE' ]; then
+		echo "****** Skipping database $db ******"
+		continue
+	fi    
 
 	GZ_FILE="$BACKUP_SAVE_FOLDER/$db.gz";
-    echo "----------------- DUMPING $db -----------------";
-    if [ $EXECUTE = true ]; then    
+	echo "----------------- DUMPING $db -----------------";
+	if [ $EXECUTE = true ]; then    
 		mysqldump -u $MUSER -h $MHOST -p$MPASS $db | gzip -9 > $GZ_FILE
-    else
-    	echo mysqldump -u $MUSER -h $MHOST -p$MPASS $db | gzip -9 > $GZ_FILE
-    fi    
+    	else
+		echo mysqldump -u $MUSER -h $MHOST -p$MPASS $db | gzip -9 > $GZ_FILE
+	fi    
     
-    echo "Finished dumping => $GZ_FILE"
+	echo "Finished dumping => $GZ_FILE"
 done
 
 echo '-------------------------------------------------------------------'
@@ -65,15 +71,15 @@ if [ -d "$LATEST" ]; then
 	if [ $EXECUTE = true ]; then
 		mv $LATEST $BACKUP_BASEFOLDER/$(date -r "$LATEST" +"%Y%m%d%H%M%S")-db
   	else
-    	echo mv $LATEST $BACKUP_BASEFOLDER/$(date -r "$LATEST" +"%Y%m%d%H%M%S")-db
+		echo mv $LATEST $BACKUP_BASEFOLDER/$(date -r "$LATEST" +"%Y%m%d%H%M%S")-db
 	fi
 fi
 
 ## Rename current run to latest ##
 if [ $EXECUTE = true ]; then
-    mv $BACKUP_SAVE_FOLDER $LATEST
+	mv $BACKUP_SAVE_FOLDER $LATEST
 else
-    echo mv $BACKUP_SAVE_FOLDER $LATEST
+	echo mv $BACKUP_SAVE_FOLDER $LATEST
 fi
 
 ## DELETE older files ##
@@ -82,16 +88,18 @@ counter=0
 
 ## List by Creation date ##
 for onefolder in $(ls -td *-db/);  do    
-    foldername=$(echo $onefolder | sed -e 's/\///g')
+	foldername=$(echo $onefolder | sed -e 's/\///g')
 	
-    if [ $counter -gt $HISTORY_STATES ]; then
-      	echo 'Removing old backup ' $BACKUP_BASEFOLDER/$foldername
-    	if [ $EXECUTE = true ]; then
-        	rm -r $BACKUP_BASEFOLDER/$foldername
-    	else
-        	echo rm -r $BACKUP_BASEFOLDER/$foldername
-    	fi
-    fi
+	if [ $counter -gt $HISTORY_STATES ]; then
+		echo 'Removing old backup ' $BACKUP_BASEFOLDER/$foldername
+    		if [ $EXECUTE = true ]; then
+        		rm -r $BACKUP_BASEFOLDER/$foldername
+    		else
+        		echo rm -r $BACKUP_BASEFOLDER/$foldername
+    		fi
+	fi
     
-    counter=$((counter+1))
+	counter=$((counter+1))
 done
+
+echo 'Finished all processes'
